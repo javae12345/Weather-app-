4f9a6b2e45576f2f71b84dfc85c8fbf7368522eb
Jack Liu <36230001@edu.cbe.ab.ca>
2023-01-17 09:45:56 -0700

    first commit

--- Weather App/src/App.svelte
+++ Weather App/src/App.svelte
@@ -1,60 +1,92 @@
 <script>
 import { time } from './lib/stores.js';
- import Geolocation from "svelte-geolocation";
+import Geolocation from "svelte-geolocation";
+
 
 let coords;
+let loaded = false
+let kelvin = 273.15;
+let cityInput = "calgary";
+
 
 const formatter = new Intl.DateTimeFormat('en', {
+
 	hour12: true,
 	hour: 'numeric',
 });
 
-const weather = {
-	value: -6,
-	description: " Mostly Cloudy",
-	city: "calgary"
+
+let weather
+$:{
+	if(!loaded&&coords&&coords[0]!=-1){
+
+		weather = getWeather(coords)
+		loaded=true
+	}
 }
 
 
-async function getWeather(){
-	let latitude = coords[1];
-	let longitude = coords[0];
+async function getWeather(c){
+	if(c){
+		let latitude = c[1];
+		let longitude = c[0];
+
 	let key = "74a2137d285b88947071a935be501d4d";
 		let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
-	console.log(api);
-	const res = await fetch(api);
-    const data = await res.json();
-    
-    if (res.ok) {
+		const res = await fetch(api);
+		const data = await res.json();
+console.log(data)
+   
         return data;
-    } 
+   } 
 }
-let promise;
-function handleClick() {
-  promise = getWeather();
+
+
+async function cityCoord(){
+	let key = "74a2137d285b88947071a935be501d4d";
+		let api = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=${key}`;
+		const res = await fetch(api);
+		const data = await res.json();
+console.log(data)
+
+   
+        return data;
+   } 
+
+
+async function handleClick() {
+let newCoords = await cityCoord()
+
+console.log(newCoords);
+
+ weather = getWeather([newCoords[0].lon, newCoords[0].lat])
 }
+
 
 </script>
-
+{#await weather then w}
 <h1 class = "title">This is the Weather Time</h1>
-
 <div id = main>
 	<div>
 		<p>{formatter.format($time)}</p>
 		
 		<Geolocation getPosition bind:coords />
-		
+
 		<button on:click = {handleClick}>
-			weather data
+			Get a new city
 		  </button>
-		<p class = city> The Weather in {weather.city} </p>
-		<p class = temperature>{weather.value}</p>
-		<p class = description> {weather.description} </p>
-		<p> Your Longitude and Latitude is {JSON.stringify(coords)}</p>
+
+		<input bind:value={cityInput} placeholder="enter a city">
+
+		<p class = city> The Weather in {w?.name} </p>
+
+		<p class = temperature>{Math.floor(w?.main.temp-kelvin)}°</p>
+
+		<p class = description> {w?.weather[0].description} </p>
 		
-	
 	</div>
 </div> 
+{/await}
 
 <style>
 
@@ -69,6 +101,9 @@ function handleClick() {
 	font-size: 40px;
 }
 
+.description {
+	font-size: 20px;
+}
 #main {
 	display: flex;
 	text-align: center;
